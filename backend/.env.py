e95032a918a3d9e358293f4097838f9c140923d2
Ed Chapman <edchapman88@gmail.com>
2024-10-12 01:40:22 +0100

    feat(env): add dynamic environment variable loading
    - Introduce .env.py for dynamic env variable configuration.
    - Utilize decouple for parsing environment variables.
    - Update settings to import configuration from .env.py.

---
+++ backend/.env.py
@@ -0,0 +1,17 @@
+import pathlib
+from functools import lru_cache
+
+from decouple import Config, RepositoryEnv
+
+BASE_DIR = pathlib.Path(__file__).resolve().parent.parent
+
+ENV_PATH = BASE_DIR.parent / ".env"
+
+@lru_cache
+def get_config(use_gcloud=True):
+    if ENV_PATH.exists():
+        return Config(RepositoryEnv(ENV_PATH))
+    from decouple import config
+    return config
+
+config = get_config()
